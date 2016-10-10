f65a6971c3bca9769b29bb43510b1235be4f876c
anushthakalia <anushthakalia@gmail.com>
2016-10-10 19:40:13 +0530

    The first,big task

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
+//to parse data to {DE: 1, HK: 1, NL: 1, UK: 1, TW: 1};
+//the first,big task
 
-var graduates = {};
+graduates={};
 
-graduates.read_static = function() {
-  var data = "greenify:DE\ndaviddao:HK\nmhelvens:NL\ntimruffles:UK\niriscshih:TW\n\n";
-  return graduates.parse(data);
+graduates.read_static=function(){
+var data="greenify:DE\ndaviddao:HK\nmhelvens:NL\ntimruffles:UK\niriscshih:TW\n\n";
+return graduates.parse(data);
 };
 
-graduates.parse = function(data) {
-  // you need to fill this
-  return {};
-};
+graduates.parse=function(data){
+var sp=data.split('\n');
+var count=0;
 
-module.exports = graduates;
+
+var final={};
+for(var i=0;i<sp.length;i++){
+if(sp[i]==''){
+	continue;
+}
+
+else{
+	var repeat=0;
+	var sp1=sp[i].split(":");
+	prop=sp1[1];
+	
+	for(var property in final){
+		if(sp1[1]==property){
+          repeat=repeat+1;
+          var prop=property;
+		}
+	}
+    final[prop]=repeat+1;
+   count=count+1;
+}
+}
+
+return final;
+}
+module.exports=graduates;
